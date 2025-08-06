24a098c98922dcaafebf3f5448cbbb3c5a6d6d32
João Ravache <138409318+MrNutsu@users.noreply.github.com>
2025-08-06 20:10:36 -0300

    Pequenas Implementações
    Implementando uso de variável referência e instanceof

--- Screen Match/src/screenmatch/main/Main.java
+++ Screen Match/src/screenmatch/main/Main.java
@@ -3,45 +3,58 @@ import screenmatch.calculations.TimeCalculator;
 import screenmatch.models.Movie;
 import screenmatch.models.Serie;
 
+import java.net.NetPermission;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
 
         ArrayList<Movie> contentList = new ArrayList<>();
+        
         //Cria a classe de tipo filme
-        Movie movie = new Movie("O Poderoso Chefão", 1972);
-
+        Movie movie = new Movie("O Poderoso Chefao", 1972);
         //Permite criar outro objeto do tipo filme, sem "repetir" a variavel Filme
         var anotherMovie = new Movie("Avatar: The way of the water",2023 );
+        var oneMoreMovie = new Movie("Nemo", 2003);
 
         movie.setDurationInMinutes(175);
         movie.setIncludedInPlan(true);
 
-//        movie.displayData();
         movie.rate(8);
         movie.rate(7.5);
         movie.rate(10);
-//        System.out.println(movie.getTotaOfRating());
-//
+
         //limita meida para 2 casas decimais
-        System.out.printf("%.2f%n", movie.getmediaRating());
-//
+        //System.out.printf("%.2f%n", movie.getmediaRating());
 
         anotherMovie.setDurationInMinutes(180);
         anotherMovie.setIncludedInPlan(false);
 
-//        anotherMovie.displayData();
         anotherMovie.rate(10);
+        anotherMovie.rate(7);
         anotherMovie.rate(10);
-        anotherMovie.rate(10);
-//        System.out.println(anotherMovie.getTotaOfRating());
-//
-//        //limita meida para 2 casas decimais
-//        System.out.printf("%.2f%n", anotherMovie.getmediaRating());
-//
-//        System.out.println("Estrelas filme 1: " + movie.getStar());
-//        System.out.println("Estrelas filme 2: " + anotherMovie.getStar());
+
+        //System.out.printf("%.2f%n",anotherMovie.getmediaRating());
+
+        oneMoreMovie.setDurationInMinutes(101);
+        oneMoreMovie.setIncludedInPlan(true);
+
+        oneMoreMovie.rate(9);
+        oneMoreMovie.rate(9);
+        oneMoreMovie.rate(8);
+
+        //System.out.printf("%.2f%n", oneMoreMovie.getmediaRating());
+
+        contentList.add(movie);
+        contentList.add(anotherMovie);
+        contentList.add(oneMoreMovie);
+
+        for(Movie item : contentList){
+                System.out.println("Nome: " + item.getName());
+                if(item instanceof Movie movies && movies.getmediaRating() > 2){
+                        System.out.println("Classificaçao: " + movies.getmediaRating() + "\n");
+                }
+        }
 
         Serie serie = new Serie("The Grand Tour", 2015);
         serie.setMinutesPerEpisode(71);
@@ -55,7 +68,7 @@ public class Main {
         TimeCalculator calculator = new TimeCalculator();
         calculator.add(movie);
         calculator.add(anotherMovie);
-        System.out.println("\n Tempo total de filmes: " + calculator.getTotalTime());
+        System.out.println("\nTempo total de filmes: " + calculator.getTotalTime());
 
 
         contentList.add(movie);
